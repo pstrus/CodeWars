ebd6aec759949824e311d2192d9b08e75b95432c
Jakub <jakub.pstrusinski@gmail.com>
2017-02-12 15:10:51 +0000

    adding FirstNonRepeatingLetter

---
+++ FirstNonRepeatingLetter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace FirstNonRepeatingLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string FirstNonRepeatingLetter(string s)
+        {
+            var ret = s.GroupBy(z => char.ToLower(z)).Where(g => g.Count() == 1).FirstOrDefault();
+            return  ret.First().ToString() ?? string.Empty;
+        }
+    }
+}
